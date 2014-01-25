9659b7d095aa538295252727013b878d74d9884b
Matthew Becker <mbecker@etcedu.com>
2014-01-25 02:35:48 -0500

    Added flashlight interaction (include Activate function in object to be activated by flashlight)... wall change color script included as example. wallcolorchange_test scene shows example.

---
+++ Assets/Wall_ChangeColor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall_ChangeColor : MonoBehaviour {
+	public Color changeColor;
+
+	public void Activate()
+	{
+		GetComponent<Renderer>().material.color = changeColor;
+	}
+}
